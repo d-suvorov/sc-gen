a68f6c04318b368adb34ca581f149d90b73a7c7a
Dmitry Suvorov <wotopul@gmail.com>
2018-12-24 01:27:32 +0300

    Merge `cancel*` events

--- blinded-auction/blinded-auction.sc
+++ blinded-auction/blinded-auction.sc
@@ -12,14 +12,14 @@ bid[1]; close[1]; reveal[1]; finish[1]
 bid[1]; close[1]; reveal[1]; finish[1]; withdraw[1]
 ;;;;
 
-cancelABB[1]
+cancel[1]
 
 
-cancelABB[1]; unbid[1]
+cancel[1]; unbid[1]
 ;
 
-bid[1]; cancelABB[1]; unbid[1]
+bid[1]; cancel[1]; unbid[1]
 ;;
 
-bid[1]; close[1]; reveal[1]; cancelRB[1]; unbid[1]
+bid[1]; close[1]; reveal[1]; cancel[1]; unbid[1]
 ;;;;
--- blinded-auction/blinded-auction.sh
+++ blinded-auction/blinded-auction.sh
@@ -4,7 +4,7 @@ for n_states in {1..5}; do
     java -jar ~/projects/EFSM-tools/jars/fast-automaton-generator.jar blinded-auction.sc \
          --ltl blinded-auction.ltl \
          --size $n_states \
-         --eventNames bid,unbid,close,cancelABB,cancelRB,reveal,finish,withdraw \
+         --eventNames bid,unbid,close,cancel,reveal,finish,withdraw \
          --result out/blinded-auction-$n_states.gv \
          --bfsConstraints --globalTree
     dot -Tpng out/blinded-auction-$n_states.gv > out/blinded_auction-$n_states.png
