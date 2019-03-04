b1d8f1d0a3df4aa447641be2bea1e2a49ca6ec32
Dmitry Suvorov <wotopul@gmail.com>
2019-03-05 00:40:22 +0300

    Add `getApproval` [WIP]

--- license/license.sc
+++ license/license.sc
@@ -62,3 +62,18 @@ getLicense[1]; use[1]; publish[1]; use[1]
 
 getLicense[1]; use[1]; publish[1]; use[1]; remove[1]
 ;;;;
+
+getLicense[1]; getApproval[1]
+;
+
+getLicense[1]; use[1]; getApproval[1]
+;;
+
+getLicense[1]; use[1]; use[1]; getApproval[1]
+;;;
+
+getLicense[1]; getApproval[1]; use[1]
+;;
+
+getLicense[1]; getApproval[1]; publish[1]
+;;
--- license/license.sh
+++ license/license.sh
@@ -4,7 +4,7 @@ for n_states in {4..8}; do
     java -jar ~/projects/EFSM-tools/jars/fast-automaton-generator.jar license.sc \
          --ltl license.ltl \
          --size $n_states \
-         --eventNames getLicense,use,publish,remove,noRemove \
+         --eventNames getLicense,getApproval,use,publish,remove,noRemove \
          --actionNames terminate \
          --result out/license-$n_states.gv \
          --bfsConstraints --globalTree
